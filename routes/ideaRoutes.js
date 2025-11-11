f9b72e0fe9418203ae14d47666e4c92c02765108
nmasters <nmasters52@gmail.com>
2025-11-11 04:12:42 -0500

    add put and delete routers

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -21,7 +21,7 @@ router.get("/", async (req, res, next) => {
 // @access              PUBLIC
 router.get("/:id", async (req, res, next) => {
   try {
-    const { id } = req.params.id;
+    const { id } = req.params;
     // check if the id exists
     if (!mongoose.Types.ObjectId.isValid(id)) {
       res.status(404);
@@ -40,7 +40,7 @@ router.get("/:id", async (req, res, next) => {
 });
 
 // @route               POST /api/ideas
-// @description         Creat new idea
+// @description         Create new idea
 // @access              Public
 router.post("/", async (req, res) => {
   try {
@@ -68,6 +68,72 @@ router.post("/", async (req, res) => {
 
     const savedIdea = await newIdea.save();
     res.status(201).json(savedIdea);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+// @route               Delete /api/ideas/:id
+// @description         Delete single ideas
+// @access              PUBLIC
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // check if the id exists
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    const idea = await Idea.findByIdAndDelete(id);
+    if (!idea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    res.json({ message: "Idea Deleted Successfully" });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+// @route               PUT /api/ideas/:id
+// @description         Update Data
+// @access              PUBLIC
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+
+    const { title, summary, description, tags } = req.body;
+
+    if (!title?.trim() || !summary?.trim() || !description?.trim()) {
+      res.status(400); // bad request
+      throw new Error("Title, Summary, and Description are required"); // this gets thrown to  middleware/errorHandler.js
+    }
+
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      id,
+      {
+        title,
+        summary,
+        description,
+        tags: Array.isArray(tags) ? tags : tags.split(",").map((t) => t.trim()),
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedIdea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+
+    res.json(updatedIdea);
   } catch (err) {
     console.log(err);
     next(err);
